f7cbb28f6acf9b77ebe2141940d2f831e6ff3d6b
Kabir Malik <31293295+k4kabirmalik@users.noreply.github.com>
2024-09-22 15:15:17 +0400

    perf: increase security by converting properties form public to internal

--- EmailSenderLibrary/EmailInfo.cs
+++ EmailSenderLibrary/EmailInfo.cs
@@ -10,25 +10,25 @@ namespace EmailSenderLibrary;
 /// <param name="subject">the subject of email</param>
 /// <param name="content">the mail body</param>
 /// <param name="files">the attachment for the email</param>
-public class EmailInfo(IEnumerable<string> mailBoxes, string subject, string content, IFormFileCollection files)
+public class EmailInfo(IEnumerable<string> mailBoxes, string subject, string content, IFormFileCollection? files)
 {
     /// <summary>
     /// The list of email recipients.
     /// </summary>
-    public List<MailAddress> SendTo { get; set; } = [.. mailBoxes.Select(x => new MailAddress(x))];
+    internal List<MailAddress> SendTo { get; set; } = [.. mailBoxes.Select(x => new MailAddress(x))];
 
     /// <summary>
     /// The subject of email
     /// </summary>
-    public string MailSubject { get; set; } = subject;
+    internal string MailSubject { get; set; } = subject;
 
     /// <summary>
     /// The body of email
     /// </summary>
-    public string MailBody { get; set; } = content;
+    internal string MailBody { get; set; } = content;
 
     /// <summary>
     /// The attachment of email
     /// </summary>
-    public IFormFileCollection Attachments { get; set; } = files;
+    internal IFormFileCollection? Attachments { get; set; } = files;
 }
